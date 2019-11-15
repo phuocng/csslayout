e8f1dae504019d9728ab923e7e91aa843bd8c4ce
Phuoc Nguyen <me@phuoc.ng>
2019-11-15 15:17:09 +0700

    Try react-snap

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { hydrate, render } from 'react-dom';
 
 import App from './App';
 
-render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+rootElement.hasChildNodes()
+    ? hydrate(<App />, rootElement)
+    : render(<App />, rootElement);
