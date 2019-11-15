3668a6ee3aaa92a87eba4eab9a995d4c2963e592
Phuoc Nguyen <me@phuoc.ng>
2019-11-15 15:40:03 +0700

    Add a button to flip between demo/source

--- client/layouts/Centering.jsx
+++ client/layouts/Centering.jsx
@@ -6,11 +6,16 @@ import BrowserFrame from '../placeholders/BrowserFrame';
 const Centering = () => {
     return (
         <DetailsLayout name="Centering">
-            <BrowserFrame>
-                <div className="h-100 flex flex-column items-center justify-center">
-                    <div className="f1 b">CENTER</div>
-                </div>
-            </BrowserFrame>
+            <BrowserFrame
+                content={
+                    <div className="h-100 flex flex-column items-center justify-center">
+                        <div className="f1 b">CENTER</div>
+                    </div>
+                }
+                source={
+                    <div>source</div>
+                }
+            />
         </DetailsLayout>
     );
 };
