b4394da9005a0fb0fb270bb23524b65a0b39c247
Phuoc Nguyen <me@phuoc.ng>
2019-11-15 17:26:10 +0700

    Add cover for sticky footer

--- client/placeholders/Frame.jsx
+++ client/placeholders/Frame.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
-const Frame = ({ children }) => {
+const Frame = ({ children, size }) => {
+    const bw = (size === 'medium' ? '2px' : '1px');
+
     return (
         <div
             className="ba b--black-30 br2"
             style={{
+                borderWidth: bw,
                 height: '100px',
                 width: '100px',
             }}
--- client/placeholders/Line.jsx
+++ client/placeholders/Line.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-const Line = () => {
+const Line = ({ size }) => {
+    const h = (size === 'medium' ? '2px' : '1px');
+
     return (
         <div
             className="w-100 bg-black-30"
-            style={{ height: '1px' }}
+            style={{ height: h }}
         />
     );
 };
