478156632fc0b8269456be262420d2567e0b67ed
Phuoc Nguyen <me@phuoc.ng>
2019-11-16 22:25:02 +0700

    Add input add-on

---
+++ client/covers/InputAddonCover.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Frame from '../placeholders/Frame';
+import Line from '../placeholders/Line';
+
+const InputAddonCover = () => {
+    return (
+        <Frame>
+            <div className="h-100 flex items-center justify-center pa2">
+                <div className="b--black-30 ba br2 flex w-100 h1">
+                    <div className="b--black-30 br flex items-center justify-center w-30 ph2">
+                        <div className="w-100">
+                        <Line />
+                        </div>
+                    </div>
+                    <div className="flex-grow-1"></div>
+                </div>
+            </div>
+        </Frame>
+    );
+};
+
+export default InputAddonCover;
