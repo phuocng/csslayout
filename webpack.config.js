0c73e56a1a1012aad6884053d690ab3d8c6ccefc
Phuoc Nguyen <me@phuoc.ng>
2019-11-23 14:38:44 +0700

    Support react router when running web dev server

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ module.exports = {
     },
     devtool: 'cheap-module-eavl-source-map',
     devServer: {
-        contentBase: path.join(__dirname, 'dist')
+        contentBase: path.join(__dirname, 'dist'),
+        historyApiFallback: true,
     },
 };
