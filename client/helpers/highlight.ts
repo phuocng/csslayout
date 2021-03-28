09151bb8c4325d060c8fd58832b3b348c0a1af82
Phuoc Nguyen <me@phuoc.ng>
2021-03-28 19:55:16 +0700

    Use Prism

--- client/helpers/highlight.ts
+++ client/helpers/highlight.ts
@@ -3,19 +3,14 @@
  * (c) 2019 - 2020 Nguyen Huu Phuoc <https://twitter.com/nghuuphuoc>
  */
 
-import hljs from 'highlight.js/lib/highlight'; // tslint:disable-line
-import javascript from 'highlight.js/lib/languages/javascript'; // tslint:disable-line
-import html from 'highlight.js/lib/languages/xml'; // tslint:disable-line
-
-hljs.registerLanguage('javascript', javascript);
-hljs.registerLanguage('html', html);
+import Prism from 'prismjs';
 
 const highlight = (input: string, language: string) => {
     const lang = language || 'html';
-    const { value } = hljs.highlight(lang, input);
+    const value = Prism.highlight(input, Prism.languages[language], language);
     const highlighted = value.replace('&amp;', '&').trim();
 
-    return `<code class="${lang}">${highlighted}</code>`;
+    return `<code class="language-${lang}">${highlighted}</code>`;
 };
 
 export default highlight;
