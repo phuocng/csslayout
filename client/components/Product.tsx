f334b7208d0d46bd9c3e5ae4dcb52c80c5ff8165
Phuoc Nguyen <me@phuoc.ng>
2021-03-30 15:09:47 +0700

    List products on sidebar

---
+++ client/components/Product.tsx
@@ -0,0 +1,27 @@
+/**
+ * A collection of popular layouts and patterns made with CSS (https://csslayout.io)
+ * (c) 2019 - 2020 Nguyen Huu Phuoc <https://twitter.com/nghuuphuoc>
+ */
+
+import * as React from 'react';
+
+import './product.css';
+import ProductModel from '../constants/ProductModel';
+import slug from '../helpers/slug';
+
+interface ProductProps {
+    product: ProductModel;
+}
+
+const Product: React.FC<ProductProps> = ({ product }) => {
+    return (
+        <div className="product">
+            <a href={product.url}>
+                <img className="product__logo" src={`/assets/${slug(product.name)}.png`} alt={`${product.name} - ${product.description}`} />
+                <div className="product__desc">{product.description}</div>
+            </a>
+        </div>
+    );
+};
+
+export default Product;
--- client/pages/ExplorePage.tsx
+++ client/pages/ExplorePage.tsx
@@ -9,7 +9,9 @@ import { Helmet } from 'react-helmet';
 import Ad from '../components/Ad';
 import CoverCard from '../components/CoverCard';
 import Heading from '../components/Heading';
+import Product from '../components/Product';
 import Pattern from '../constants/Pattern';
+import { ProductList } from '../constants/ProductList';
 import useDocumentTitle from '../hooks/useDocumentTitle';
 import Layout from '../layouts/Layout';
 import './explorePage.css';
@@ -32,133 +34,144 @@ const ExplorePage = () => {
             </div>
 
             <div className="container">
-                <Ad />
+                <div className="content">
+                    <main class="main">
+                        <section>
+                            <Heading title="Layout" />
 
-                <section>
-                    <Heading title="Layout" />
+                            <div className="explore__collection">
+                                <CoverCard pattern={Pattern.CardLayout} />
+                                <CoverCard pattern={Pattern.HolyGrail} />
+                                <CoverCard pattern={Pattern.SameHeightColumns} />
+                                <CoverCard pattern={Pattern.Sidebar} />
+                                <CoverCard pattern={Pattern.SimpleGrid} />
+                                <CoverCard pattern={Pattern.SplitScreen} />
+                                <CoverCard pattern={Pattern.StickyFooter} />
+                                <CoverCard pattern={Pattern.StickyHeader} />
+                                <CoverCard pattern={Pattern.StickySections} />
+                            </div>
+                        </section>
 
-                    <div className="explore__collection">
-                        <CoverCard pattern={Pattern.CardLayout} />
-                        <CoverCard pattern={Pattern.HolyGrail} />
-                        <CoverCard pattern={Pattern.SameHeightColumns} />
-                        <CoverCard pattern={Pattern.Sidebar} />
-                        <CoverCard pattern={Pattern.SimpleGrid} />
-                        <CoverCard pattern={Pattern.SplitScreen} />
-                        <CoverCard pattern={Pattern.StickyFooter} />
-                        <CoverCard pattern={Pattern.StickyHeader} />
-                        <CoverCard pattern={Pattern.StickySections} />
+                        <section>
+                            <Heading title="Navigation" />
+
+                            <div className="explore__collection">
+                                <CoverCard pattern={Pattern.Breadcrumb} />
+                                <CoverCard pattern={Pattern.CircularNavigation} />
+                                <CoverCard pattern={Pattern.DotNavigation} />
+                                <CoverCard pattern={Pattern.Drawer} />
+                                <CoverCard pattern={Pattern.Dropdown} />
+                                <CoverCard pattern={Pattern.FullScreenMenu} />
+                                <CoverCard pattern={Pattern.MegaMenu} />
+                                <CoverCard pattern={Pattern.Menu} />
+                                <CoverCard pattern={Pattern.NestedDropdowns} />
+                                <CoverCard pattern={Pattern.Pagination} />
+                                <CoverCard pattern={Pattern.PreviousNextButtons} />
+                                <CoverCard pattern={Pattern.SplitNavigation} />
+                                <CoverCard pattern={Pattern.Tab} />
+                                <CoverCard pattern={Pattern.Wizard} />
+                            </div>
+                        </section>
+
+                        <section>
+                            <Heading title="Input" />
+
+                            <div className="explore__collection">
+                                <CoverCard pattern={Pattern.ButtonWithIcon} />
+                                <CoverCard pattern={Pattern.Chip} />
+                                <CoverCard pattern={Pattern.CustomCheckboxButton} />
+                                <CoverCard pattern={Pattern.CustomRadioButton} />
+                                <CoverCard pattern={Pattern.FloatingLabel} />
+                                <CoverCard pattern={Pattern.InputAddon} />
+                                <CoverCard pattern={Pattern.RadioButtonGroup} />
+                                <CoverCard pattern={Pattern.RadioSwitch} />
+                                <CoverCard pattern={Pattern.Rating} />
+                                <CoverCard pattern={Pattern.SearchBox} />
+                                <CoverCard pattern={Pattern.Slider} />
+                                <CoverCard pattern={Pattern.SpinButton} />
+                                <CoverCard pattern={Pattern.StepperInput} />
+                                <CoverCard pattern={Pattern.Switch} />
+                                <CoverCard pattern={Pattern.TogglePasswordVisibility} />
+                                <CoverCard pattern={Pattern.UploadButton} />
+                            </div>
+                        </section>
+
+                        <section>
+                            <Heading title="Display" />
+
+                            <div className="explore__collection">
+                                <CoverCard pattern={Pattern.Accordion} />
+                                <CoverCard pattern={Pattern.ArrowButtons} />
+                                <CoverCard pattern={Pattern.Avatar} />
+                                <CoverCard pattern={Pattern.AvatarList} />
+                                <CoverCard pattern={Pattern.Badge} />
+                                <CoverCard pattern={Pattern.Card} />
+                                <CoverCard pattern={Pattern.Centering} />
+                                <CoverCard pattern={Pattern.CloseButton} />
+                                <CoverCard pattern={Pattern.CookieBanner} />
+                                <CoverCard pattern={Pattern.CornerRibbon} />
+                                <CoverCard pattern={Pattern.CurvedBackground} />
+                                <CoverCard pattern={Pattern.DiagonalSection} />
+                                <CoverCard pattern={Pattern.DockedAtCorner} />
+                                <CoverCard pattern={Pattern.DotLeader} />
+                                <CoverCard pattern={Pattern.DropArea} />
+                                <CoverCard pattern={Pattern.DropCap} />
+                                <CoverCard pattern={Pattern.FadingLongSection} />
+                                <CoverCard pattern={Pattern.FeatureComparison} />
+                                <CoverCard pattern={Pattern.FeatureList} />
+                                <CoverCard pattern={Pattern.FixedAtCorner} />
+                                <CoverCard pattern={Pattern.FixedAtSide} />
+                                <CoverCard pattern={Pattern.FullBackground} />
+                                <CoverCard pattern={Pattern.InitialAvatar} />
+                                <CoverCard pattern={Pattern.KeyboardShortcut} />
+                                <CoverCard pattern={Pattern.LinedPaper} />
+                                <CoverCard pattern={Pattern.MediaObject} />
+                                <CoverCard pattern={Pattern.OverlayPlayButton} />
+                                <CoverCard pattern={Pattern.PricingTable} />
+                                <CoverCard pattern={Pattern.PropertyList} />
+                                <CoverCard pattern={Pattern.QuestionsAndAnswers} />
+                                <CoverCard pattern={Pattern.Ribbon} />
+                                <CoverCard pattern={Pattern.Separator} />
+                                <CoverCard pattern={Pattern.StackedCards} />
+                                <CoverCard pattern={Pattern.StampBorder} />
+                                <CoverCard pattern={Pattern.Statistic} />
+                                <CoverCard pattern={Pattern.StatusLight} />
+                                <CoverCard pattern={Pattern.StickyTableColumn} />
+                                <CoverCard pattern={Pattern.StickyTableHeaders} />
+                                <CoverCard pattern={Pattern.Teardrop} />
+                                <CoverCard pattern={Pattern.Timeline} />
+                                <CoverCard pattern={Pattern.TriangleButtons} />
+                                <CoverCard pattern={Pattern.VideoBackground} />
+                                <CoverCard pattern={Pattern.Watermark} />
+                            </div>
+                        </section>
+
+                        <section>
+                            <Heading title="Feedback" />
+
+                            <div className="explore__collection">
+                                <CoverCard pattern={Pattern.Modal} />
+                                <CoverCard pattern={Pattern.Notification} />
+                                <CoverCard pattern={Pattern.PopoverArrow} />
+                                <CoverCard pattern={Pattern.ProgressBar} />
+                                <CoverCard pattern={Pattern.RadialProgressBar} />
+                                <CoverCard pattern={Pattern.ResizableElement} />
+                                <CoverCard pattern={Pattern.PresenceIndicator} />
+                                <CoverCard pattern={Pattern.Tooltip} />
+                                <CoverCard pattern={Pattern.ValidationIcon} />
+                            </div>
+                        </section>
+                    </main>
+
+                    <div className="sidebar">
+                        <div className="sidebar__inner">
+                            <Ad />
+                            {
+                                ProductList.map(product => <Product key={product.name} product={product} />)
+                            }
+                        </div>
                     </div>
-                </section>
-
-                <section>
-                    <Heading title="Navigation" />
-
-                    <div className="explore__collection">
-                        <CoverCard pattern={Pattern.Breadcrumb} />
-                        <CoverCard pattern={Pattern.CircularNavigation} />
-                        <CoverCard pattern={Pattern.DotNavigation} />
-                        <CoverCard pattern={Pattern.Drawer} />
-                        <CoverCard pattern={Pattern.Dropdown} />
-                        <CoverCard pattern={Pattern.FullScreenMenu} />
-                        <CoverCard pattern={Pattern.MegaMenu} />
-                        <CoverCard pattern={Pattern.Menu} />
-                        <CoverCard pattern={Pattern.NestedDropdowns} />
-                        <CoverCard pattern={Pattern.Pagination} />
-                        <CoverCard pattern={Pattern.PreviousNextButtons} />
-                        <CoverCard pattern={Pattern.SplitNavigation} />
-                        <CoverCard pattern={Pattern.Tab} />
-                        <CoverCard pattern={Pattern.Wizard} />
-                    </div>
-                </section>
-
-                <section>
-                    <Heading title="Input" />
-
-                    <div className="explore__collection">
-                        <CoverCard pattern={Pattern.ButtonWithIcon} />
-                        <CoverCard pattern={Pattern.Chip} />
-                        <CoverCard pattern={Pattern.CustomCheckboxButton} />
-                        <CoverCard pattern={Pattern.CustomRadioButton} />
-                        <CoverCard pattern={Pattern.FloatingLabel} />
-                        <CoverCard pattern={Pattern.InputAddon} />
-                        <CoverCard pattern={Pattern.RadioButtonGroup} />
-                        <CoverCard pattern={Pattern.RadioSwitch} />
-                        <CoverCard pattern={Pattern.Rating} />
-                        <CoverCard pattern={Pattern.SearchBox} />
-                        <CoverCard pattern={Pattern.Slider} />
-                        <CoverCard pattern={Pattern.SpinButton} />
-                        <CoverCard pattern={Pattern.StepperInput} />
-                        <CoverCard pattern={Pattern.Switch} />
-                        <CoverCard pattern={Pattern.TogglePasswordVisibility} />
-                        <CoverCard pattern={Pattern.UploadButton} />
-                    </div>
-                </section>
-
-                <section>
-                    <Heading title="Display" />
-
-                    <div className="explore__collection">
-                        <CoverCard pattern={Pattern.Accordion} />
-                        <CoverCard pattern={Pattern.ArrowButtons} />
-                        <CoverCard pattern={Pattern.Avatar} />
-                        <CoverCard pattern={Pattern.AvatarList} />
-                        <CoverCard pattern={Pattern.Badge} />
-                        <CoverCard pattern={Pattern.Card} />
-                        <CoverCard pattern={Pattern.Centering} />
-                        <CoverCard pattern={Pattern.CloseButton} />
-                        <CoverCard pattern={Pattern.CookieBanner} />
-                        <CoverCard pattern={Pattern.CornerRibbon} />
-                        <CoverCard pattern={Pattern.CurvedBackground} />
-                        <CoverCard pattern={Pattern.DiagonalSection} />
-                        <CoverCard pattern={Pattern.DockedAtCorner} />
-                        <CoverCard pattern={Pattern.DotLeader} />
-                        <CoverCard pattern={Pattern.DropArea} />
-                        <CoverCard pattern={Pattern.DropCap} />
-                        <CoverCard pattern={Pattern.FadingLongSection} />
-                        <CoverCard pattern={Pattern.FeatureComparison} />
-                        <CoverCard pattern={Pattern.FeatureList} />
-                        <CoverCard pattern={Pattern.FixedAtCorner} />
-                        <CoverCard pattern={Pattern.FixedAtSide} />
-                        <CoverCard pattern={Pattern.FullBackground} />
-                        <CoverCard pattern={Pattern.InitialAvatar} />
-                        <CoverCard pattern={Pattern.KeyboardShortcut} />
-                        <CoverCard pattern={Pattern.LinedPaper} />
-                        <CoverCard pattern={Pattern.MediaObject} />
-                        <CoverCard pattern={Pattern.OverlayPlayButton} />
-                        <CoverCard pattern={Pattern.PricingTable} />
-                        <CoverCard pattern={Pattern.PropertyList} />
-                        <CoverCard pattern={Pattern.QuestionsAndAnswers} />
-                        <CoverCard pattern={Pattern.Ribbon} />
-                        <CoverCard pattern={Pattern.Separator} />
-                        <CoverCard pattern={Pattern.StackedCards} />
-                        <CoverCard pattern={Pattern.StampBorder} />
-                        <CoverCard pattern={Pattern.Statistic} />
-                        <CoverCard pattern={Pattern.StatusLight} />
-                        <CoverCard pattern={Pattern.StickyTableColumn} />
-                        <CoverCard pattern={Pattern.StickyTableHeaders} />
-                        <CoverCard pattern={Pattern.Teardrop} />
-                        <CoverCard pattern={Pattern.Timeline} />
-                        <CoverCard pattern={Pattern.TriangleButtons} />
-                        <CoverCard pattern={Pattern.VideoBackground} />
-                        <CoverCard pattern={Pattern.Watermark} />
-                    </div>
-                </section>
-
-                <section>
-                    <Heading title="Feedback" />
-
-                    <div className="explore__collection">
-                        <CoverCard pattern={Pattern.Modal} />
-                        <CoverCard pattern={Pattern.Notification} />
-                        <CoverCard pattern={Pattern.PopoverArrow} />
-                        <CoverCard pattern={Pattern.ProgressBar} />
-                        <CoverCard pattern={Pattern.RadialProgressBar} />
-                        <CoverCard pattern={Pattern.ResizableElement} />
-                        <CoverCard pattern={Pattern.PresenceIndicator} />
-                        <CoverCard pattern={Pattern.Tooltip} />
-                        <CoverCard pattern={Pattern.ValidationIcon} />
-                    </div>
-                </section>
+                </div>
             </div>
         </Layout>
     );
