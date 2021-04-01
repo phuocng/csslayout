354f8da849a43832690bb78bb0cb693378e74fae
Phuoc Nguyen <me@phuoc.ng>
2021-04-01 15:13:01 +0700

    Update header

--- client/helpers/highlight.ts
+++ client/helpers/highlight.ts
@@ -1,6 +1,6 @@
 /**
  * A collection of popular layouts and patterns made with CSS (https://csslayout.io)
- * (c) 2019 - 2020 Nguyen Huu Phuoc <https://twitter.com/nghuuphuoc>
+ * (c) 2019 - 2021 Nguyen Huu Phuoc <https://twitter.com/nghuuphuoc>
  */
 
 import Prism from 'prismjs';
