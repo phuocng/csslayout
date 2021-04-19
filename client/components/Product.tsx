692e3a2adb4e92b984f674fc1270251fb1a66342
Phuoc Nguyen <me@phuoc.ng>
2021-04-19 21:59:10 +0700

    Tweak sidebar

--- client/components/Product.tsx
+++ client/components/Product.tsx
@@ -15,9 +15,10 @@ interface ProductProps {
 
 const Product: React.FC<ProductProps> = ({ product }) => {
     return (
-        <div className="product">
+        <div className="product" style={{ backgroundColor: product.themeColor }}>
             <a href={product.url}>
                 <img className="product__logo" src={`/assets/${slug(product.name)}.png`} alt={`${product.name} - ${product.description}`} />
+                <h3 className="product__name">{product.name}</h3>
                 <div className="product__desc">{product.description}</div>
             </a>
         </div>
