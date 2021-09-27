829d57c1bf7bcc8ef1adba178af06a8b47fc65a0
Phuoc Nguyen <me@phuoc.ng>
2021-09-27 23:33:13 +0700

    Cleanup

--- patterns/fixed-at-corner/Cover.tsx
+++ patterns/fixed-at-corner/Cover.tsx
@@ -1,8 +1,3 @@
-/**
- * A collection of popular layouts and patterns made with CSS (https://csslayout.io)
- * (c) 2019 - 2021 Nguyen Huu Phuoc <https://twitter.com/nghuuphuoc>
- */
-
 import * as React from 'react';
 
 import Frame from '../../placeholders/Frame';
