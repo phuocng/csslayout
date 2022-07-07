1af52908e71bd7a64e35e0ed6a4faadab5b0c257
ndom91 <yo@ndo.dev>
2022-07-07 18:37:19 +0200

    feat: apply removeIndent helper to docked-at-corner' Code output

--- pages/docked-at-corner/index.tsx
+++ pages/docked-at-corner/index.tsx
@@ -1,12 +1,13 @@
-import * as React from 'react';
-import Head from 'next/head';
-import { Heading, Spacer } from '@1milligram/design';
+import * as React from 'react'
+import Head from 'next/head'
+import { Heading, Spacer } from '@1milligram/design'
 
-import { RelatedPatterns } from '../../components/RelatedPatterns';
-import { Pattern } from '../../constants/Pattern';
-import { PatternLayout } from '../../layouts/PatternLayout';
-import BrowserFrame from '../../placeholders/BrowserFrame';
-import Rectangle from '../../placeholders/Rectangle';
+import { RelatedPatterns } from '../../components/RelatedPatterns'
+import { Pattern } from '../../constants/Pattern'
+import { PatternLayout } from '../../layouts/PatternLayout'
+import BrowserFrame from '../../placeholders/BrowserFrame'
+import Rectangle from '../../placeholders/Rectangle'
+import { removeIndent } from '../../utils/removeIndent'
 
 const Details: React.FC<{}> = () => {
     return (
@@ -28,7 +29,7 @@ const Details: React.FC<{}> = () => {
     ...
 </div>
 `}
-                css={`
+                css={removeIndent`
                     .container {
                         position: relative;
                     }
@@ -155,7 +156,7 @@ const Details: React.FC<{}> = () => {
             <Spacer size="extraLarge" />
             <RelatedPatterns patterns={[Pattern.PresenceIndicator]} />
         </PatternLayout>
-    );
-};
+    )
+}
 
-export default Details;
+export default Details
