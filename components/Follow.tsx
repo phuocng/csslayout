80d924aeaf7a5ed1e21c7ed3d88d7b2dfa675513
Phuoc Nguyen <me@phuoc.ng>
2022-09-22 10:19:48 +0700

    feat: Banner

--- components/Follow.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-
-export const Follow = () => (
-    <div className="block-follow">
-        If you find this site helpful, follow me on <a href="https://twitter.com/nghuuphuoc">Twitter</a> and{' '}
-        <a href="https://github.com/phuocng">GitHub</a> to get more useful content like this.
-    </div>
-);
